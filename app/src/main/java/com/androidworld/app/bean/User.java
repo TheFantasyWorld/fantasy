5ee03146c184d9a95180c8c5f62cdebe0346a80b
LQC <565718255@qq.com>
2016-06-08 16:40:44 +0800

    Tests

--- app/src/main/java/com/androidworld/app/bean/User.java
+++ app/src/main/java/com/androidworld/app/bean/User.java
@@ -9,4 +9,12 @@ package com.androidworld.app.bean;
 public class User {
 
     private String userName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 }
